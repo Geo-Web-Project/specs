1c065801b285c6637f75cf9b6361d084731d1b20
Cody Hatfield <cody.hatfield@me.com>
2021-09-17 15:01:53 -0700

    Add Media Gallery definition

--- ceramic/definitions/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for i in *.json; do
-    idx definition:create me --name="$(cat $i | jq -r '.name')" --description="$(cat $i | jq -r '.description')" --schema="$(cat $i | jq -r '.schema')"
-done
